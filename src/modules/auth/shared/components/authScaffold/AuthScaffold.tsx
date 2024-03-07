fa5862bbf948611b4343413fa4dee40c24c5adfc
Samuel Rodrigues da Silva <samuka_55@hotmail.com>
2024-03-07 07:24:21 -0300

    refactor: Improving auth layout content positioning

--- src/modules/auth/shared/components/authScaffold/AuthScaffold.tsx
+++ src/modules/auth/shared/components/authScaffold/AuthScaffold.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { ReactNode } from 'react';
-import { Image, View } from 'react-native';
+import { Image, ImageSourcePropType, View } from 'react-native';
 import { getAlignment } from 'src/modules/shared/style/SharedProcessedStyle';
+import ExPressable from '../buttons/pressable/ExPressable';
 
 const stylesheet = {
-  view: 'flex grow bg-primary p-4 gap-y-4',
+  view: 'flex flex-col grow bg-primary p-4 gap-y-4 pb-14',
   header: 'my-6 self-start',
 };
 
@@ -13,17 +14,48 @@ interface AuthScaffoldProps {
   justify?: string;
   hasArrowBack?: boolean;
   children: ReactNode;
+  ctaPrimary?: () => void;
+  ctaPrimaryText?: string;
+  ctaPrimaryLeftIcon?: ImageSourcePropType;
+  ctaSecondary?: () => void;
+  ctaSecondaryText?: string;
+  ctaSecondaryLeftIcon?: ImageSourcePropType;
 }
 
 const AuthScaffold = ({
   hasArrowBack = true,
   children,
   alignment = 'items-start',
+  ctaPrimary,
+  ctaPrimaryText,
+  ctaPrimaryLeftIcon,
+  ctaSecondary,
+  ctaSecondaryText,
+  ctaSecondaryLeftIcon,
 }: AuthScaffoldProps) => {
   return (
     <View className={`${stylesheet.view} ${getAlignment(alignment)}`}>
       <StatusBar />
+
       {children}
+
+      <View className='w-full gap-y-4'>
+        {ctaPrimary && ctaPrimaryText && (
+          <ExPressable
+            title={ctaPrimaryText}
+            icon={ctaPrimaryLeftIcon}
+            onPress={ctaPrimary}
+          ></ExPressable>
+        )}
+        {ctaSecondary && ctaSecondaryText && (
+          <ExPressable
+            title={ctaSecondaryText}
+            colorScheme='light'
+            onPress={ctaSecondary}
+            icon={ctaSecondaryLeftIcon}
+          ></ExPressable>
+        )}
+      </View>
     </View>
   );
 };
